572bfba786a5b69c0dda5d5b044fc1d0a966ff02
zana-a <zana200.a@gmail.com>
2021-08-12 17:21:14 +0100

    add example to examples folder

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import consola from 'consola';
 import { Either } from 'fp-ts/lib/Either';
 import { run } from 'parser-ts/code-frame';
+import * as FS from 'fs';
+import * as Path from 'path';
 import * as Parsers from './parser';
 
-function parseAll<P, Q>(result: Either<P, Q>) {
-  if (result._tag === 'Left') {
-    consola.error('');
-    consola.log(result.left);
-  } else consola.log(result.right);
+function runner(path: string) {
+  function result<P, Q>(object: Either<P, Q>) {
+    if (object._tag === 'Left') {
+      consola.error('');
+      consola.log(object.left);
+    } else consola.log(object.right);
+  }
+
+  FS.readFile(Path.normalize(path), 'utf-8', (err, data) => {
+    if (err) throw err;
+    result(run(Parsers.Binding, data.toString()));
+  });
 }
 
-// parseAll(run(Parsers.Ident, '__main__'));
-parseAll(run(Parsers.Binding, 'hi = 2'));
+runner('./examples/main.qi');
