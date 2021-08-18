e9ac55ab216705f316bfc4b0bf2cee78c3230f87
zana-a <zana200.a@gmail.com>
2021-08-18 14:01:22 +0100

    add copyright

--- src/parser/block.ts
+++ src/parser/block.ts
@@ -1,3 +1,8 @@
+// Copyright Qi Lang 2021. All Rights Reserved.
+// Node module: qi-lang-core
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import * as P from 'arcsecond';
 import * as Parsers from './index';
 import * as Symbols from './symbols';
--- src/parser/spacey1.ts
+++ src/parser/spacey1.ts
@@ -1,3 +1,8 @@
+// Copyright Qi Lang 2021. All Rights Reserved.
+// Node module: qi-lang-core
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import * as P from 'arcsecond';
 
 const Spacey1 = (x: P.Parser<any, any, any>) => P.between(
