366c2fbd99b5d75b36c88c7f0dce17efd863df34
zana-a <zana200.a@gmail.com>
2021-08-29 00:46:03 +0100

    update kind

---
+++ src/structure/kind.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Qi Lang. 2021 All Rights Reserved.
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+/**
+ * * There seems to be a lot of issues with Eslint and TS Enums.
+ * * Hence the eslint disable flags, naturally.
+ */
+
+/* eslint-disable no-unused-vars */
+
+// eslint-disable-next-line no-shadow
+enum Kind {
+  Ident,
+  Binding,
+  Bool,
+  Atom,
+  Number,
+  List,
+  Tuple,
+  Map,
+  Pair,
+  Module,
+  Fn,
+  Function,
+  Param,
+  Block,
+  String,
+  Lambda
+}
+
+export default Kind;
