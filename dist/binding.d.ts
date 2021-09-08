48897203f715568e0f7018149eb381b5283079a0
zana-a <zana200.a@gmail.com>
2021-09-09 00:20:38 +0100

    remove dist

--- dist/binding.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Arc from 'arcsecond';
-export declare namespace Binding {
-    namespace Parser {
-        const object: Arc.Parser<any[], string, any>;
-    }
-    namespace Structure {
-    }
-}
--- dist/lambda.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Arc from 'arcsecond';
-export declare namespace Lambda {
-    namespace Parser {
-        const object: Arc.Parser<any[], string, any>;
-    }
-    namespace Structure {
-    }
-}
