b4ef4b65288a4ee8c4825b001f89ae946c070406
zana-a <zana200.a@gmail.com>
2021-09-23 17:37:01 +0100

    remove .d.ts files

--- types/program.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Arc from 'arcsecond';
-export declare namespace Program {
-    namespace Parser {
-        const object: Arc.Parser<unknown, string, any>;
-    }
-    namespace Structure {
-    }
-}
--- types/type/string.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Arc from 'arcsecond';
-export declare namespace String {
-    namespace Parser {
-        const object: Arc.Parser<string, string, any>;
-    }
-    namespace Structure {
-    }
-}
