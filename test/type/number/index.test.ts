0435d3e92b38e94eec5bace1be17ebd0aca892a6
zana-a <zana200.a@gmail.com>
2021-09-23 20:00:34 +0100

    add number parser placeholders

---
+++ test/type/number/index.test.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Qi Lang. 2021 All Rights Reserved.
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+import { fail } from 'assert';
+import { describe, it } from 'mocha';
+
+describe('number parser', () => {
+  it('should parse an integer', () => {
+    fail();
+  });
+
+  it('should parse a float', () => {
+    fail();
+  });
+});
