a1a77ada0113217dc120dfa33829475eca0496f6
zana-a <zana200.a@gmail.com>
2021-10-14 15:05:52 +0100

    add assign to the body of def

--- src/parsers/definition.rs
+++ src/parsers/definition.rs
@@ -1,7 +1,7 @@
 /*
  * Copyright Qi Lang. 2021 All Rights Reserved.
- * This file is licensed under the MIT License.
- * License text available at https://opensource.org/licenses/MIT
+ * Th ident: todo!(), params: todo!(), body: todo!() is file is licensed under the MIT License.
+ * License text available at https://opensource. ident: todo!(), body: todo!() org/licenses/MIT
  */
 
 use crate::parsers;
@@ -11,7 +11,7 @@ use crate::symbols::raw;
 pub struct Definition {
     pub ident: parsers::ident::Ident,
     pub params: Option<parsers::parameter::Parameters>,
-    pub body: Option<DefinitionBody>,
+    pub body: Option<Vec<DefinitionBody>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -20,18 +20,15 @@ pub enum DefinitionBody {
     Definition(Box<parsers::definition::Definition>),
 }
 
-pub fn get_definition(input: &str) -> nom::IResult<&str, DefinitionBody> {
-    let (input, result) = parsers::definition::parse(input)?;
-    Ok((input, DefinitionBody::Definition(Box::new(result))))
-}
-
 pub fn get_assign(input: &str) -> nom::IResult<&str, DefinitionBody> {
     let (input, result) = parsers::assign::parse(input)?;
     Ok((input, DefinitionBody::Assign(result)))
 }
 
 pub fn get_body(input: &str) -> nom::IResult<&str, DefinitionBody> {
-    nom::branch::alt((get_definition, get_assign))(input)
+    nom::branch::alt((
+        get_assign, get_assign, /* Change to another type of statement */
+    ))(input)
 }
 
 pub fn parse(input: &str) -> nom::IResult<&str, Definition> {
@@ -57,7 +54,7 @@ pub fn parse(input: &str) -> nom::IResult<&str, Definition> {
             nom::sequence::preceded(
                 nom::character::complete::multispace1,
                 nom::combinator::opt(nom::sequence::terminated(
-                    get_body,
+                    nom::multi::separated_list1(nom::character::complete::multispace1, get_body),
                     nom::character::complete::multispace1,
                 )),
             ),
@@ -73,4 +70,62 @@ pub fn parse(input: &str) -> nom::IResult<&str, Definition> {
             body: result.1,
         },
     ))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::parsers;
+
+    #[test]
+    fn test_empty_definition() {
+        let input = "def hello do
+        end";
+
+        let result = parsers::definition::parse(input);
+        assert_eq!(
+            result,
+            Ok((
+                "",
+                parsers::definition::Definition {
+                    ident: parsers::ident::Ident {
+                        body: "hello".to_string()
+                    },
+                    params: None,
+                    body: None,
+                }
+            ))
+        )
+    }
+
+    #[test]
+    fn test_definition_with_assign_body() {
+        let input = "def hello do 
+            a = true
+        end";
+
+        let result = parsers::definition::parse(input);
+        assert_eq!(
+            result,
+            Ok((
+                "",
+                parsers::definition::Definition {
+                    ident: parsers::ident::Ident {
+                        body: "hello".to_string()
+                    },
+                    params: None,
+                    body: Some(vec![parsers::definition::DefinitionBody::Assign(
+                        parsers::assign::Assign {
+                            ident: parsers::ident::Ident {
+                                body: "a".to_string()
+                            },
+                            body: parsers::assign::AssignBody::Bool(parsers::bool::Bool {
+                                body: true
+                            })
+                        }
+                    )]),
+                }
+            ))
+        )
+    }
 }
